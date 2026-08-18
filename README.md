89cdadb973fe9337ab8ea2d6d1073f29b830b075
Flávio Araújo Filho <fagpaf@cin.ufpe.br>
2026-08-17 22:30:36 -0300

    colocando as anotações sobre JOIN da aula

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# SQL
+# CS50 SQL
+
+Repositório criado para acompanhar meus estudos no curso **CS50's Introduction to Databases with SQL**, da Harvard University.
+
+## Sobre
+
+Neste repositório estão os exercícios e anotações que estou desenvolvendo durante o curso para aprender os fundamentos de **SQL e bancos de dados**.
+
+## Conteúdos a serem estudados
+
+* Consultas SQL
+* Filtragem e ordenação de dados
+* Funções de agregação
+* `GROUP BY` e `HAVING`
+* Subconsultas
+* `JOIN`
+* Chaves primárias e estrangeiras
+* Relacionamentos entre tabelas
+* Normalização
+* `INSERT`, `UPDATE` e `DELETE`
+* Views
+* Índices
+* Otimização de consultas
+* Modelagem de bancos de dados
+* SQLite
+* PostgreSQL
+* MySQL
+
+
+## Curso
+
+[CS50's Introduction to Databases with SQL](https://cs50.harvard.edu/sql/)
--- aula1.sql
+++ aula1.sql
@@ -41,7 +41,7 @@
 --     WHERE "title" = 'In Memory of Memory'
 -- );
 
--- Todas as consultas acima são de 1---1
+-- Todas as consultas acima são de 1-para-1
 -- ============================================
 
 -- SELECT "name"
@@ -57,16 +57,56 @@
 -- );
 
 -- Palavra chave "IN", aceita subconsultas que retornam 0, 1 ou N.
--- Sendo de cardinalidade 1----N
+-- Sendo de cardinalidade 1-para-N
 
-SELECT "title"
-FROM "books"
-WHERE "id" IN (
-    SELECT "book_id"
-    FROM "authored"
-    WHERE "author_id" = (
-        SELECT "id"
-        FROM "authors"
-        WHERE "name" = 'Fernanda Melchor'
-    )
-);
+
+-- Para entender subconsultas aninhadas em SQL, a melhor forma é lê-las de dentro para fora
+
+-- Consulta a tabela "books" e filtra apenas as linhas cujo "id" pertence à lista obtida no passo 2, exibindo a coluna "title"
+-- SELECT "title"
+-- FROM "books"
+-- WHERE "id" IN (
+--     SELECT "book_id"
+    -- Vai até a tabela associativa de n-para-n "authored" e busca todos os "book_id" onde o author_id é o ID retornado no passo 1.
+    -- FROM "authored"
+    -- WHERE "author_id" = (
+    --     SELECT "id"
+    --     FROM "authors"
+    --     WHERE "name" = 'Fernanda Melchor'
+        -- Varre a tabela "authors" procurando pelo registro com o nome 'Fernanda Melchor' e devolve o seu "id"
+-- );
+
+-- Subconsultas n-para-n acima
+-- ====================================================
+
+
+-- JOIN
+
+-- SELECT * 
+-- FROM sea_lions
+-- JOIN migrations ON migrations.id = sea_lions.id;
+
+-- Uso de aspas duplas: Usar somente quando o nome tiver espaços ou seja uma palavra reservada
+
+-- LEFT JOIN
+SELECT * 
+FROM sea_lions
+LEFT JOIN migrations ON migrations.id = sea_lions.id;
+
+
+-- RIGHT JOIN
+SELECT *
+FROM sea_lions
+RIGHT JOIN migrations ON migrations.id = sea_lions.id;
+
+
+-- FULL JOIN
+SELECT * 
+FROM sea_lions
+FULL JOIN migrations ON migrations.id = sea_lions.id;
+
+
+-- NATURAL JOIN 
+SELECT * 
+FROM sea_lions
+NATURAL JOIN migrations;
