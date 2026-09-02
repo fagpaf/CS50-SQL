1e23fa80ed3b862c4ff170872843693ed2e75c35
Flavio Alexandrino Guedes Pereira de Araujo Filho <fagpaf@WINDOWS.CIN.UFPE.BR>
2026-09-02 16:50:00 -0300

    finalizando o dese problem

--- dese/10.sql
+++ dese/10.sql
@@ -2,5 +2,6 @@ SELECT name, per_pupil_expenditure
 FROM districts
 JOIN expenditures
     ON districts.id = expenditures.district_id
+WHERE districts.type = 'Public School District' 
 ORDER BY per_pupil_expenditure DESC
 LIMIT 10;
---
+++ dese/12.sql
@@ -0,0 +1,12 @@
+SELECT name, per_pupil_expenditure, exemplary
+FROM districts
+JOIN expenditures
+    ON districts.id = expenditures.district_id
+JOIN staff_evaluations
+    ON districts.id = staff_evaluations.district_id
+WHERE per_pupil_expenditure > (
+    SELECT AVG(per_pupil_expenditure)
+    FROM expenditures
+)
+AND exemplary IS NOT NULL
+ORDER BY (exemplary/100) DESC, per_pupil_expenditure DESC;
