b8e2b31e57bd1e16fccc4fafb6c073f53e745672
Flávio Araújo Filho <fagpaf@cin.ufpe.br>
2026-09-24 21:38:04 -0300

    finalizando a lista 3

--- pset3/meteorites/import.sql
+++ pset3/meteorites/import.sql
@@ -11,7 +11,7 @@ CREATE TABLE "meteorites_temp" (
     PRIMARY KEY ("id")
 );
 
--- .import --csv --skip 1 meteorites.csv meteorites_temp
+.import --csv --skip 1 meteorites.csv meteorites_temp
 
 UPDATE "meteorites_temp" SET "mass" = NULL WHERE "mass" = '';
 UPDATE "meteorites_temp" SET "year" = NULL WHERE "year" = '';
@@ -19,25 +19,29 @@ UPDATE "meteorites_temp" SET "lat"  = NULL WHERE "lat"  = '';
 UPDATE "meteorites_temp" SET "long" = NULL WHERE "long" = '';
 -------------------------------------------------------------
 
-UPDATE "meteorites_temp" SET "mass" ROUND("mass", 2) WHERE "mass" IS NOT NULL;
-UPDATE "meteorites_temp" SET "lat"  ROUND("lat", 2)  WHERE "lat"  IS NOT NULL;
-UPDATE "meteorites_temp" SET "long" ROUND("long", 2) WHERE "long" IS NOT NULL;
+UPDATE "meteorites_temp" SET "mass" = ROUND("mass", 2) WHERE "mass" IS NOT NULL;
+UPDATE "meteorites_temp" SET "lat"  = ROUND("lat", 2)  WHERE "lat"  IS NOT NULL;
+UPDATE "meteorites_temp" SET "long" = ROUND("long", 2) WHERE "long" IS NOT NULL;
 ------------------------------------------------------------------------------
 
 -- Aqui eu faço a tabela do modo correto, do modo q vai ser usado de vdd
 CREATE TABLE "meteorites" (
-    "id" INTEGER AUTOINCREMENT,
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
-    "nametype" TEXT NOT NULL,
     "class" TEXT NOT NULL,
     "mass" REAL,
     "discovery" TEXT NOT NULL,
     "year" REAL,
     "lat" REAL,
-    "long" REAL,
-    PRIMARY KEY ("id")
+    "long" REAL
 );
 
--- Na inserção eu ordeno e retiro as linhas q contém 'Relict'
+-- Na inserção eu ordeno e retiro as linhas q contém 'Relict', PQ no INSERT eu posso utilizar SELECT
 
-INSERT INTO 
+INSERT INTO "meteorites" ("name", "class", "mass", "discovery", "year", "lat", "long")
+SELECT "name", "class", "mass", "discovery", "year", "lat", "long"
+FROM "meteorites_temp"
+WHERE "nametype" != 'Relict'
+ORDER BY "year" ASC, "name" ASC;
+
+DROP TABLE meteorites_temp;
